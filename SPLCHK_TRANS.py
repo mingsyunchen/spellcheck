681aa1390ebbc46dea9a0e141ac11db988b20a7d
syun <106572020+mingsyunchen@users.noreply.github.com>
2022-08-08 23:37:13 +0800

    Update SPLCHK_TRANS.py

--- SPLCHK_TRANS.py
+++ SPLCHK_TRANS.py
@@ -1,13 +1,14 @@
 import streamlit as st
+st.write('splchk_key:')
+splchk_key = input()
+st.write('trans_key:')
+trans_key = input()
+
 from spellcheck import spellcheck as sc
 from translate import trans_zhHant, trans_jp, trans_ko
 
 st.title('Spell Check')
 
-st.write('splchk_key:')
-splchk_key = input()
-st.write('trans_key:')
-trans_key = input()
 text = st.text_input('在這裡試著輸入一段英文句子吧:','write something here~')
 text1 = sc(text)
 st.write('修正後的句子: {}'.format(text1))
